8f8f991eb74bb7a2e41fe4a2e8a7dd1998d3bb05
user10 <work20go@mail.ru>
2023-01-19 13:24:53 +0200

    look at me Hector

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -8,5 +8,14 @@ internal class Program
         Complex complex2 = new Complex(3,11);
         Complex complex3 = (Complex)complex1.Add(complex2);
         Console.WriteLine(complex3.ToString());
+        Complex complex4 = (Complex)complex1.Sub(complex2);
+        Console.WriteLine(complex4.ToString());
+        Complex complex5 = (Complex)complex1.Mul(complex2);
+        Console.WriteLine(complex5.ToString());
+        Complex complex6 = (Complex)complex1.Div(complex2);
+        Console.WriteLine(complex6.ToString());
+        Console.WriteLine(complex1.Equ(complex2));
+        Complex complex7 = (Complex)complex2.Conj();
+        Console.WriteLine(complex7.ToString());
     }
 }
