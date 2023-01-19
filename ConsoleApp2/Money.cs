d2c73221da7ec04efe7a4dd3a99b37968667e8f2
user10 <work20go@mail.ru>
2023-01-19 12:41:34 +0200

    ye

--- ConsoleApp2/Money.cs
+++ ConsoleApp2/Money.cs
@@ -18,21 +18,38 @@ namespace ConsoleApp2
             {
                 return new Money(this.A + p.A+1, (this.B + p.B) -100);
             }
+            else return new Money(this.A + p.A + 1, (this.B + p.B));
         }
 
         public override Pair Div(Pair p)
         {
-            return this.A / this.B;
+                return new Money(this.A / p.A, this.B / p.B);
         }
 
         public override Pair Mul(Pair p)
         {
-            throw new NotImplementedException();
+            if (this.B * p.B < 100)
+            {
+                return new Money(this.A * p.A, this.B * p.B);
+            }
+            else
+            {
+                double temp = this.B * p.B / 100;
+                return new Money(this.A * p.A + temp, (this.B * p.B) - temp * 100);
+            }
         }
 
         public override Pair Sub(Pair p)
         {
-            throw new NotImplementedException();
+            if (this.A - p.A >= 0 && this.B - p.B < 0)
+            {
+                return new Money(this.A - p.A - 1, ((this.B + 100) - p.B));
+            }
+            else if (this.A - p.A < 0)
+            {
+                return new Money(0, 0);
+            }
+            else return new Money(this.A + p.A + 1, (this.B + p.B));
         }
     }
 }
